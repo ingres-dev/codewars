7dd6df49b6c976d6eb05591dfe92d52e1cdab65f
Francisco Leonardo <148999121+ingres-dev@users.noreply.github.com>
2025-12-05 23:16:08 -0300

    Implement method to sum multiples of 3 and 5

---
+++ somaMultiplos3e5.java
@@ -0,0 +1,12 @@
+public class Solution {
+
+  public int solution(int number) {
+    int soma = 0;
+        for (int i = 1; i < number; i++) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                soma += i;
+            }
+        }
+        return soma;
+  }
+}
